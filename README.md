aa1c0ab9eef00b61f58cafa85364ea76373b3ccb
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-05 19:45:46 -0700

    init commit. init typescript, tsc

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meals 4 Aid
+
+Meals 4 Aid is a full stack, single page application built with React + TypeScript
---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <h1>Meals 4 Aid</h1>
+      </main>
+    </>
+  );
+};
+export default App;
