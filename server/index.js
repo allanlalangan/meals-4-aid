3439a6a4046a2d48a48d25f4d31ad03e3d84bdf2
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-05 23:48:37 -0700

    setup basic server, connect db, proxy

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { connectDB } = require('./config/db');
+
+// SERVER START
+const app = express();
+app.use(cors());
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`server on port ${port}`);
+});
+
+// CONNECT TO DATABASE
+connectDB();
+
+// PARSE BODY MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// FALLBACK ROUTE
+app.use('*', (req, res) => {
+  res.status(404).json({ error: 'not found' });
+});
