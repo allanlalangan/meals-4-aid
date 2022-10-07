d17d807e8a4add37a48359c349bd19b406a4c3c9
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-06 23:22:07 -0700

    npm i react-icons add Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,21 @@ import HomePage from './pages/home';
 import DashboardPage from './pages/dashboard';
 import LoginPage from './pages/login';
 import RegisterPage from './pages/register';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <header>
-          <h1>Meals 4 Aid</h1>
-        </header>
-        <main className='flex'>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
-            <Route path='/dashboard' element={<DashboardPage />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className='flex'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 };
 export default App;
